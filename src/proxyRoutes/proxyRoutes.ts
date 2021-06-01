cb65699d538ed1ff22fc02ccf20ad16df60cb582
Nicky Janse <nicky.janse.nj@gmail.com>
2021-06-01 13:32:27 +0200

    env for proxy addresses

--- src/proxyRoutes/proxyRoutes.ts
+++ src/proxyRoutes/proxyRoutes.ts
@@ -2,35 +2,42 @@ import ProxyRoute from "./proxyRoute";
 
 export class AuthProxy implements ProxyRoute {
 	public path: string = "/auth";
-	public target: string = "http://localhost:3001";
+	public target: string = process.env.AUTH_SERVICE_HOST
+		? process.env.AUTH_SERVICE_HOST
+		: "http://localhost:3001";
 }
-
-// export class MailProxy implements ProxyRoute {
-// 	public path: string = "/mail";
-// 	public target: string = "http://localhost:3002"
-// }
 
 export class ProfileProxy implements ProxyRoute {
 	public path: string = "/profile";
-	public target: string = "http://localhost:3003";
+	public target: string = process.env.PROFILE_SERVICE_HOST
+		? process.env.PROFILE_SERVICE_HOST
+		: "http://localhost:3003";
 }
 
 export class TrendProxy implements ProxyRoute {
 	public path: string = "/trend";
-	public target: string = "http://localhost:3004";
+	public target: string = process.env.TREND_SERVICE_HOST
+		? process.env.TREND_SERVICE_HOST
+		: "http://localhost:3004";
 }
 
 export class KweetProxy implements ProxyRoute {
 	public path: string = "/kweet";
-	public target: string = "http://localhost:3005";
+	public target: string = process.env.KWEET_SERVICE_HOST
+		? process.env.KWEET_SERVICE_HOST
+		: "http://localhost:3005";
 }
 
 export class FollowProxy implements ProxyRoute {
 	public path: string = "/follow";
-	public target: string = "http://localhost:3006";
+	public target: string = process.env.FOLLOW_SERVICE_HOST
+		? process.env.FOLLOW_SERVICE_HOST
+		: "http://localhost:3006";
 }
 
 export class LikeProxy implements ProxyRoute {
 	public path: string = "/like";
-	public target: string = "http://localhost:3007";
+	public target: string = process.env.LIKE_SERVICE_HOST
+		? process.env.LIKE_SERVICE_HOST
+		: "http://localhost:3007";
 }
